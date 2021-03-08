c037ea32a9e5b6f9a324987bcf24850435562bc8
Yana Vakhrameeva <yana4cus@gmail.com>
2021-03-08 11:26:17 +0300

    Added Main

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String [] args){
+        int account_balance = 2_000_000_000;
+        int transfer_amount = 500_000_000;
+        int total = account_balance + transfer_amount;
+        System.out.println("Total:"+ total);
+    }
+
+}
+·
